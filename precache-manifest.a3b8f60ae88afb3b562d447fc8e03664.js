393dc8eb32324871ffc5d35b40eacc3674b85e6f
Maleriandro <marianolazzarini01@gmail.com>
2025-08-29 04:36:38 +0000

    Deploying to gh-pages from @ Maleriandro/MyAnimeTimeline@df59121bc1e32ca7e6d7e810a21f436bbfc6aa93 🚀

---
+++ precache-manifest.a3b8f60ae88afb3b562d447fc8e03664.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa75ad49cc819ab1a7cdeebee31a38a1",
+    "url": "/MyAnimeTimeline/index.html"
+  },
+  {
+    "revision": "d93cd1b0ebf3b5beb217",
+    "url": "/MyAnimeTimeline/static/css/main.39e6e995.chunk.css"
+  },
+  {
+    "revision": "3b639564d956cd50f81d",
+    "url": "/MyAnimeTimeline/static/js/2.7390038d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyAnimeTimeline/static/js/2.7390038d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93cd1b0ebf3b5beb217",
+    "url": "/MyAnimeTimeline/static/js/main.6a83055c.chunk.js"
+  },
+  {
+    "revision": "feb25eb2de995adebf09d7175d0eac51",
+    "url": "/MyAnimeTimeline/static/js/main.6a83055c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b97b6485a15dfb5c0f",
+    "url": "/MyAnimeTimeline/static/js/runtime-main.42f707c6.js"
+  }
+]);
